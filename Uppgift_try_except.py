945ba1e3f9c6344aa5f66ad757c2d8a58a94b541
Tobbz92 <tobiashjortsberg@hotmail.com>
2026-04-01 14:15:16 +0200

    Update Uppgift_try_except.py

--- Uppgift_try_except.py
+++ Uppgift_try_except.py
@@ -28,18 +28,23 @@ while True:
         # Hanterar tal som matas in med fel format och omvandlar till flyttal
         tal_1 = float(tal_1.replace(',', '.'))  
         tal_2 = float(tal_2.replace(',', '.'))
-
-        result = calculate(tal_1, op, tal_2)
         
-        if isinstance(result, str):
-            # Skriver ut felmeddelande om result är en text
-            print(result)
-        else:
-        # Om result inte är en text skrivs detta ut
-            print(f'{tal_1} {op} {tal_2} = {result}')
-    # Hanterar division med 0
-    except ZeroDivisionError:
-        print('Error: Division med 0')
     # Hanterar fel antal delar, fel operator och felaktig operand
     except ValueError:
         print('Felaktig inmatning')
+    else:
+        try:
+            result = calculate(tal_1, op, tal_2)
+        
+            if isinstance(result, str):
+                # Skriver ut felmeddelande om result är en text
+                print(result)
+            else:
+            # Om result inte är en text skrivs detta ut
+                print(f'{tal_1} {op} {tal_2} = {result}')
+                
+        # Hanterar division med 0
+        except ZeroDivisionError:
+            print('Error: Division med 0')
+
+
